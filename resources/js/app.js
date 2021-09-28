3bbae868cb65ce010079702426eefee6140b1a3a
Floy <dmitrievd1995@mail.ru>
2021-09-29 00:03:19 +0300

    fc

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,89 @@
-require('./bootstrap');
+import('../sass/app.scss')
+
+window._ = require('lodash');
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+let game = {
+
+    init(){
+        this.handleGenerateGameButton()
+        this.handleSignInButton()
+    },
+
+    handleSignInButton(){
+        let button = document.querySelector(`._sign_in_game`)
+        if (!button) return;
+        button.addEventListener(`click`, (event)=>{
+
+            let data = this.validateSignInData(this.getSignInData())
+            if (!data.success) { alert('Не вверные данные'); return; }
+
+            axios.get(`/game/${data.id}`,{
+            })
+                .then((response) => {
+                    console.log(response)
+                })
+                .catch(reason => {
+                    alert('Игра с указанным ID не существует')
+                })
+        })
+    },
+
+    validateSignInData(data){
+        if (data.id.value === ''){
+            data.success = false
+        }
+        return {id: data.id.value, success: data.success}
+    },
+
+    getSignInData(){
+        let success = true
+        let id = document.querySelector(`#id`)
+        if (!id) success = false
+        return {id: id, success: success}
+    },
+
+    handleGenerateGameButton(){
+        let button = document.querySelector(`._generate_game`)
+        if (!button) return;
+        button.addEventListener(`click`, (event)=>{
+
+            let data = this.validateGenerateFormData(this.getGenerateFormData())
+            if (!data.success) { alert('Не вверные данные'); return; }
+
+            axios.post('/game',{
+                width: data.width,
+                height: data.height,
+            })
+                .then((response) => {
+                    let id = document.querySelector(`._id_container`)
+                    if (!id) return;
+
+                    id.innerHTML = response.data.id
+                })
+                .catch(reason => {
+                    alert('Ширина или высота не попадают в диапазоны минимального и максильного значения или четные')
+                })
+        })
+    },
+
+    getGenerateFormData(){
+        let success = true
+        let width = document.querySelector(`#width`)
+        let height = document.querySelector(`#height`)
+        if (!width || !height) success = false
+        return {width: width, height: height, success: success}
+    },
+
+    validateGenerateFormData(data){
+        if (data.width.value === '' || data.height.value === ''){
+            data.success = false
+        }
+        return {width: data.width.value, height: data.height.value, success: data.success}
+    }
+}
+
+game.init()
+
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,40 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
     <title>Document</title>
 </head>
-<body>
-    lol
+<body class="main">
+
+    <div class="wrapper center column vertical_center">
+        <h1 class="logo">© Floy TYZ</h1>
+
+        <label for="width" class="label">Width
+            <input type="text" id="width" placeholder="width" required>
+        </label>
+
+        <label for="height" class="label">Height
+            <input type="text" id="height" placeholder="height" required>
+        </label>
+
+        <button class="button _generate_game">Generate game</button>
+
+        <p class="_id_container"></p>
+    </div>
+
+    <hr>
+
+    <div class="wrapper center column vertical_center">
+
+        <label for="id" class="label">Id
+            <input type="text" id="id" placeholder="id" required>
+        </label>
+
+        <button class="button _sign_in_game">Sign in game</button>
+    </div>
+
 </body>
+
+<script src="/js/app.js"></script>
+
 </html>

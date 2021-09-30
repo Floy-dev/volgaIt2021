2cef8f7d7b0a63fadee2b4a35779fc1ad1d62bb6
floy <floy-dev@mail.ru>
2021-09-30 13:31:05 +0300

    fast commit

--- resources/views/room_cells.blade.php
+++ resources/views/room_cells.blade.php
@@ -1,10 +1,6 @@
 @for ($i = 0; $i < $height; $i++)
-
     @for ($j = 0; $j < $width; $j++, $inc++)
-        <div class="block {{ $cells[$inc]['playerId'] == $currentPlayerId ? 'block__current' : ''}}" style="background-color: {{ $cells[$inc]['color'] }}"></div>
-
+        <div class="block {{ $i % 2 == 0 ? 'odd' : 'even' }} {{ $cells[$inc]['playerId'] == $currentPlayerId ? 'block__current' : ''}}" style="background-color: {{ $cells[$inc]['color'] }}"></div>
     @endfor
-
     <br>
-
 @endfor
